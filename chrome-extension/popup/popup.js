40d4b95665a4aadddcc86fd82724a7aa6d5419d1
ikset <ikset@ubuntu.(none)>
2013-01-26 01:04:59 +0100

    new design and functionality. New version.

--- chrome-extension/popup/popup.js
+++ chrome-extension/popup/popup.js
@@ -3,6 +3,10 @@
 // found in the LICENSE file.
 
 function start(){
+    $("#rizzomaLink").click(function() {
+        var urlToMentionBlip = "https://www.rizzoma.com/topic/";
+        window.open(urlToMentionBlip, "_blank");
+    });
     getUnreadMentionsAndDisplayThem();
 }
 
@@ -13,13 +17,30 @@ function getUnreadMentionsAndDisplayThem() {
 function askForUnreadMentions(callback) {
     chrome.extension.sendMessage("GET_NEWEST_MENTIONS", callback);
 }
-
+function minimizePopup(){
+    $('#notifications').css({height: '10px'});
+    $('body').css({height: '10px'});
+    $('body').css({styleFloat: 'right'});
+    $('#notifications').css({width: '290px'});
+    $('body').css({width: '290px'});
+}
 function displayMentions(mentions) {
+    var readedMentions = new Array();
+    var anreadedMentions = new Array();
+    if (mentions.length == 0)
+        minimizePopup();
     for (var i = 0; i < mentions.length; i++) {
         var mention = mentions[i];
         if (!mention.isRead)
-            displayMention(mention);
+          anreadedMentions.push(mention);
+        else
+          readedMentions.push(mention);
     }
+    for (i = 0; i < anreadedMentions.length; i++)
+        displayMention(anreadedMentions[i]);
+    var n = Math.min(30 - anreadedMentions.length, readedMentions.length);
+    for (i = 0; i < n; i++)
+        displayMention(readedMentions[i]);
 }
 
 function displayMention(mention) {
@@ -29,15 +50,40 @@ function displayMention(mention) {
         window.open(urlToMentionBlip, "_blank");
     })
 }
-
+function getTimeString(time)
+{
+    // "Fri, 25 Jan 2013 16:29:06 GMT"
+    var timeStr = time.toString();
+    var timeNow = (new Date(Date.now())).toString();
+    var timeArray = timeStr.split(' ');
+    var resultTime;
+    if (!(timeNow.split(' ')[1] == timeArray[1] 
+        && timeNow.split(' ')[2] == timeArray[2])) { 
+        resultTime = timeArray[1] + " " + timeArray[2];
+    } else {
+        var hm = timeArray[4].split(':');
+        resultTime = hm[0] + ":" + hm[1];
+    }
+    return resultTime;
+}
 function addNotification(mention, callback){
     var text = mention.title;
+    var b=new Date(Date.now());
+    var lSent = parseInt(mention.lastSent)*1000 + b.getTimezoneOffset();
+    var date=new Date(lSent);
+    //var date = new Date(1000 * parseInt(mention.lastSent));
+    var timeStr = getTimeString(date);
+    var backGroundColor;
+    //console.log(timeStr);
     var avatarUrl = mention.senderAvatar;
+    if (avatarUrl == null)
+        avatarUrl = "https://rizzoma.com/s/img/user/unknown.png";
     var name = mention.senderName;
+    var readStatus = mention.isRead;
     var nfs = document.getElementById('notifications');
     var k = $('.notification').size() + 1;
-    if (text.length > 45){
-        text = text.substring(0, 40) + " ... ";
+    if (text.length > 200){
+        text = text.substring(0, 200) + " ... ";
     }
     var id  = 'notification'+ k;
     var str ='<div  class="notification" id="'+ id +'"><span class="snipets" >' 
@@ -47,16 +93,30 @@ function addNotification(mention, callback){
     var imgStr = '<img class="avatar" title ="' 
             + name
             +'" src="' + avatarUrl
-            +'"height="24" width="24">';
-    console.log($('#notifications'));
+            +'"height="40" width="40">';
+    // console.log($('#notifications'));
+    var timeDivStr = '<div class="date" title ="' 
+            + date.toString()
+            +'">' 
+            + timeStr
+            +'</div>';
     id = '#' + id;
     $(id).append(imgStr);
+    if (readStatus){
+        $(id).css({fontWeight: 'normal'});
+        backGroundColor = "white";
+    }
+    else {
+        $(id).css({fontWeight: 'bold'});
+        backGroundColor = "white";
+    }
+    $(id).append(timeDivStr);
     $(id).mouseover(function(){
         $(id).css({backgroundColor: '#3184D6'});
     });
     
     $(id).mouseout(function(){
-        $(id).css({backgroundColor: "rgb(227,234,240)"});
+        $(id).css({backgroundColor: backGroundColor});
     });
     $(id).click(callback);
 }
--- chrome-extension/src/background-page/draw-image.js
+++ chrome-extension/src/background-page/draw-image.js
@@ -2,19 +2,24 @@
   updateNumberOfNotifications(5);  
 });*/
 function updateNumberOfNotifications(num){
+    if (num == 0) return;
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
     // ...draw to the canvas...
     var img = new Image();
     img.onload = function(){
       context.drawImage(this, 0, 0, 19, 19);
-      context.font = "bold 10pt Arial";
+      context.font = "bold 7pt Arial";
+      context.fillStyle = "white";
       if (num != 0)
-        context.fillText(num, 3, 14);  // 10, 16 auch ok?
+        if (num < 10)
+          context.fillText(num, 10, 17);  // 10, 16 auch ok?
+        else
+          context.fillText(num, 7, 17);  // 10, 16 auch ok?
       var imageData = context.getImageData(0, 0, 19, 19);
       chrome.browserAction.setIcon({
         imageData: imageData
       });
     };
-    img.src = "../rizzoma.png";
+    img.src = "../icon21x21.png";
 }
