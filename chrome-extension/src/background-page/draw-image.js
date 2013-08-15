f25859fbd1d29c8e48874518f4f70265fa30bc42
Robin Tibor Schirrmeister <robintibor@gmail.com>
2013-08-15 21:54:06 +0200

    now showing normal rizzoma symbol when no unread mentions

--- chrome-extension/src/background-page/draw-image.js
+++ chrome-extension/src/background-page/draw-image.js
@@ -2,24 +2,29 @@
   updateNumberOfNotifications(5);  
 });*/
 function updateNumberOfNotifications(num){
-    if (num == 0) return;
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    // ...draw to the canvas...
-    var img = new Image();
-    img.onload = function(){
-      context.drawImage(this, 0, 0, 19, 19);
-      context.font = "bold 7pt Arial";
-      context.fillStyle = "white";
-      if (num != 0)
+    if (num == 0) {
+      // show the the normal rizzoma icon without numbers
+      chrome.browserAction.setIcon({path: '../../img/rizzoma-48x48.png'});
+    } else {
+      var canvas = document.getElementById('canvas');
+      var context = canvas.getContext('2d');
+      // ...draw to the canvas...
+      var img = new Image();
+      img.onload = function(){
+        context.drawImage(this, 0, 0, 19, 19);
+        context.font = "bold 7pt Arial";
+        context.fillStyle = "white";
+        // Write number at correct position depending 
+        // if number has one or two digits
         if (num < 10)
           context.fillText(num, 10, 17);
         else
           context.fillText(num, 7, 17);
-      var imageData = context.getImageData(0, 0, 19, 19);
-      chrome.browserAction.setIcon({
-        imageData: imageData
-      });
-    };
-    img.src = "../../img/unread-mentions-icon.png";
+        var imageData = context.getImageData(0, 0, 19, 19);
+        chrome.browserAction.setIcon({
+          imageData: imageData
+        });
+      };
+      img.src = "../../img/unread-mentions-icon.png";
+    }
 }
